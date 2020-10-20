352d6ec5a04cbe0fc373f2e186021d9760b60f7d
mkzo <61697292+mkzo@users.noreply.github.com>
2020-10-20 18:01:47 -0400

    Add files via upload

--- mymalloc.c
+++ mymalloc.c
@@ -35,25 +35,25 @@ void* mymalloc(int req) {
                 new_nd->magicNumber = 329851398;
                 nd->size = req;
                 nd->used = true;
-                return nd;
+                return (char*)nd+sizeof(Node);
             }
             /* Just enough space to allocate this block */
             else {
                 nd->used = true;
-                return nd;
+                return (char*)nd+sizeof(Node);
             }
         }
         else {
             if ((char*)nd + nd->size > (myblock+4095)) {
                 printf("Not enough space");
-                return first;
+                return (char*)first;
             }
             printf("Checking next block\n");
             nd = (Node*)((char*)nd + nd->size + sizeof(Node));
         }
     }
     /* Error has occured */
-    return first;
+    return (char*)first;
 }
 
 void myfree(void* ptr) {
@@ -65,57 +65,60 @@ void myfree(void* ptr) {
         printf("Specified address is not a pointer");
         return;
     }
-    Node* nd1;
+    Node* nd1 = first;
     Node* nd2 = first;
     while ((char*) nd2 < (char*) ptr) {
-        if ((char*)nd2 + nd2->size > (myblock+4095)) {
-                printf("Specified pointer has not been malloced");
-                return;
-            }
-            printf("Checking next block\n");
-            if (nd2 == first) {
-                nd1 = first;
-            }
-            else {
-                nd1 = (Node*)((char*)nd1 + nd1->size + sizeof(Node));
-            }
-            nd2 = (Node*)((char*)nd2 + nd2->size + sizeof(Node));
-    }
-    if ((char*) nd2 == (char*) ptr) {
-        printf("Specified pointer found...clearing");
-        /* Free procedure */
-        nd2->used = false;
-        if (nd1->used == false) {
-            /* Merge with previous memory block if possible */
-            nd1->size = nd1->size + nd2->size + sizeof(Node);
-        }
-        nd1 = nd2; // hopefully this doesn't translate changes to nd2 into nd1 too
-        if ((char*)nd2 + nd2->size <= (myblock+4095)) {
-            /* If possible, increment nd2 to the next memory block */
-            nd2 = (Node*)((char*)nd2 + nd2->size + sizeof(Node));
-            if (nd2->used == false) {
-                /* Merge with next memory block */
+        printf("nd %p, %p\n", nd2, ptr);
+        // if ((char*)nd2 + sizeof(Node) + nd2->size > (myblock+4095)) {
+        //         printf("Specified pointer has not been malloced");
+        //         return;
+        //     }
+        if ((char*) nd2 + sizeof(Node) == (char*) ptr) {
+            printf("Specified pointer found...clearing\n");
+            /* Free procedure */
+            nd2->used = false;
+            if (nd1->used == false) {
+                /* Merge with previous memory block if possible */
                 nd1->size = nd1->size + nd2->size + sizeof(Node);
             }
+            nd1 = nd2; // Hopefully this doesn't translate changes to nd2 into nd1 too
+            if ((char*)nd2 + nd2->size <= (myblock+4095)) {
+                /* If possible, increment nd2 to the next memory block */
+                nd2 = (Node*)((char*)nd2 + nd2->size + sizeof(Node));
+                if (nd2->used == false) {
+                    /* Merge with next memory block */
+                    nd1->size = nd1->size + nd2->size + sizeof(Node);
+                }
+            }
+            return;
         }
-        return;
+        else if ((char*) nd2 > (char*) ptr) {
+            printf("Specified pointer has not been malloced\n");
+            return;
+        }
+        else {
+            printf("Checking next block\n");
+                if (nd2 == first) {
+                    nd1 = first;
+                }
+                else {
+                    nd1 = (Node*)((char*)nd1 + nd1->size + sizeof(Node));
+                }
+                nd2 = (Node*)((char*)nd2 + nd2->size + sizeof(Node));
+        }
     }
-    if ((char*) nd2 > (char*) ptr) {
-        printf("Specified pointer has not been malloced");
-        return;
-    }
+    return;
 }
 
 /* For testing purposes, in reality our functions would be included in a library */
 int main() {
-    char* c = mymalloc(1);
+    printf("%p\n",myblock);
+    char* c = mymalloc(10);
     printf("%p\n",c);
     char* d = mymalloc(10);
     printf("%p\n",d);
-    char* e = mymalloc(5);
-    printf("%p\n",e);
-    free(c);
-    c = mymalloc(1);
+    myfree(c);
+    c = mymalloc(10);
     printf("%p\n",c);
     return 0;
 }
