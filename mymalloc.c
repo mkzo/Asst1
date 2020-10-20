8338067b7e6e0cc8174321a5b06a719bc0f652fd
Sunny Chen <sunny@Sunnys-MacBook-Pro.local>
2020-10-20 16:11:48 -0400

    aa

--- mymalloc.c
+++ mymalloc.c
@@ -2,65 +2,115 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
-#define SIZE 30
-static char myblock[SIZE];
+#define BLOCK_S 30
+#define META_S 2
+static unsigned char myblock[BLOCK_S];
 
+void print_block(void *addr);
 
+// 1 means valid/free, 0 means in use
+int meta_get_valid(void *addr) {
+    unsigned char mask = 1 << 7;
+    unsigned char val = *(unsigned char *)addr;
+    return (mask & val) >> 7;
+}
 
+int meta_get_size(void *addr) {
+    unsigned char *b1 = addr;
+    unsigned char *b2 = addr+1;
 
-// void* mymalloc(int req) {
-//     /* Initializes first node, should only run once */
-//     if (first == NULL) {
-//         first = (Node*)myblock;
-//         first->size = SIZE-sizeof(Node);
-//         first->used = false;
-//     }
-//     /* Assign new memory block */
-//     Node* nd = first;
-//     while (true) { 
-//         /* Free block and enough space to accomodate request */
-//         if (nd->used == false && nd->size >= req) {
-//             /* Enough space to make a new block */
-//             if (nd->size > req + sizeof(Node)) {
-//                 Node* new_nd = (Node*)((char*)nd + sizeof(Node) + req);
-//                 new_nd->size = nd->size - req -sizeof(Node);
-//                 new_nd->used = false;
-//                 nd->size = req;
-//                 nd->used = true;
-//                 printf("%p %p\n", nd, nd+sizeof(Node));
-//                 return nd+sizeof(Node);
-//             }
-//             /* Just enough space to allocate this block */
-//             else {
-//                 nd->used = true;
-//                 return nd+sizeof(Node);
-//             }
-//         }
-//         else {
-//             if ((char*)nd + nd->size > (myblock+SIZE-1)) {
-//                 printf("Not enough space");
-//                 return first;
-//             }
-//             nd = (Node*)((char*)nd + nd->size + sizeof(Node));
-//         }
-//     }
-//     /* Error has occured */
-//     return first;
-// }
+    int val = 0;
+    val += *b2;
+
+    unsigned char mask = 0xf;
+    val += ((*b1) & mask) << 8;
+
+    return val;
+}
+
+void meta_set_valid(void *addr, int valid) {
+    *(unsigned char *)addr |= valid << 7;
+}
+
+void meta_set_size(void *addr, int size) {
+    unsigned char *b1 = addr;
+    unsigned char *b2 = addr+1;
+
+    int mask1 = 0xf00;
+    int mask2 = 0x0ff;
+
+    // save valid because we will overwrite
+    int valid = meta_get_valid(addr);
+    *b1 = (size & mask1) >> 8;
+    meta_set_valid(addr, valid);
+
+    *b2 = (size & mask2);
+}
+
+void *mymalloc(int size) {
+    /* Initializes first node, should only run once */
+    if (meta_get_size(myblock) == 0) {
+        meta_set_valid(myblock, 1);
+        meta_set_size(myblock, BLOCK_S - META_S);
+    }
+
+    /* Assign new memory block */
+    void *nd = myblock;
+    while (nd - (void *)myblock < BLOCK_S) { 
+        int nd_valid = meta_get_valid(nd);
+        int nd_size = meta_get_size(nd);
+        /* Free block and enough space to accomodate request */
+        if (nd_valid == 1 && nd_size >= size) {
+            /* Enough space to make a new block */
+            if (nd_size > size + META_S) {
+                void *new_nd = nd + (size + META_S);
+                meta_set_valid(new_nd, 1);
+                meta_set_size(new_nd, nd_size - (size + META_S));
+
+                meta_set_valid(nd, 0);
+                meta_set_size(nd, size);
+
+                return nd+META_S;
+            }
+            /* Just enough space to allocate this block */
+            else {
+                meta_set_valid(nd, 0);
+                return nd+META_S;
+            }
+        }
+
+        else {
+            nd += nd_size + META_S;
+        }
+    }
+    /* Error has occured */
+    printf("NO SPACE\n");
+    return NULL;
+}
 
 void print_mem() {
-    for (int i=0; i < SIZE; i++) {
+    for (int i=0; i < META_S; i++) {
         printf("%d ", myblock[i]);
     }
     printf("\n");
 }
 
+void print_block(void *addr) {
+    int res = meta_get_valid(addr);
+    int size = meta_get_size(addr);
+    printf("Block at %p:\n\tValid: %d\n\tSize: %d\n\n", addr, res, size);
+}
 
 /* For testing purposes, in reality our functions would be included in a library */
 int main() {
+    print_block(myblock);
 
-    printf("%x\n", log(10));
+    mymalloc(10);
+
+    print_block(myblock);
+    print_block(myblock+11);
 
     return 0;
 }
