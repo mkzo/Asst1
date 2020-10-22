b6cac64cf61c04d14d4e69bed9e137a02010f7e1
Sunny Chen <sunnychen2000@gmail.com>
2020-10-22 17:25:13 -0400

    fml

--- mymalloc.c
+++ mymalloc.c
@@ -2,42 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <math.h>
+#include "mymalloc.h"
 
-#define BLOCK_SIZE 2000
+#define BLOCK_SIZE 30
 #define META_SIZE 2
 static char myblock[BLOCK_SIZE];
 
-void print_block(char *addr);
-
-
 // 1 means in use, 0 means free
-int get_used(char *addr) {
-    unsigned char mask = 1 << 7;  // 10000000
-    unsigned char val = *addr;
+bool get_used(void *addr) {
+    unsigned char mask = 0x1 << 7;  // 10000000
+    unsigned char val = *(unsigned char*)addr;
     return (mask & val) >> 7;
 }
 
-int get_size(char *addr) {
-    unsigned char *b1 = addr;
-    unsigned char *b2 = addr+1;
+int get_size(void *addr) {
+    unsigned char *b1 = (unsigned char*)addr;
+    unsigned char *b2 = (unsigned char*)addr+1;
 
     int val = 0;
     char mask = 0xf;
+
     val += ((*b1) & mask) << 8;
     val += *b2;
+
     return val;
 }
 
-void set_used(char *addr, int used) {
+int get_freed(void *addr) {
+    unsigned char mask = 0x3 << 4;  // 00110000
+    unsigned char val = *(unsigned char*)addr;
+    return (mask & val) >> 4;
+}
+
+void set_used(void *addr, bool used) {
     if (get_used(addr) != used) {
-        *addr ^= 1 << 7;
+        *(unsigned char*)addr ^= 1 << 7;
     }
 }
 
-void set_size(char *addr, int size) {
-    unsigned char *b1 = addr;
-    unsigned char *b2 = addr+1;
+void set_size(void *addr, int size) {
+    unsigned char *b1 = (unsigned char*)addr;
+    unsigned char *b2 = (unsigned char*)addr+1;
 
     int mask1 = 0xf00;
     int mask2 = 0x0ff;
@@ -50,6 +55,16 @@ void set_size(char *addr, int size) {
     *b2 = (size & mask2);
 }
 
+void set_freed(void *addr, int size) {
+    unsigned char mask = 0x3 << 4; // 00110000
+    
+    unsigned char val = *(unsigned char*)addr;
+    val &= ~mask;
+    val |= (size << 4);
+
+    *(unsigned char*)addr = val;
+}
+
 void *mymalloc(int size) {
     /* Initializes first node, should only run once */
     if (get_size(myblock) == 0) {
@@ -57,35 +72,35 @@ void *mymalloc(int size) {
         set_size(myblock, BLOCK_SIZE - META_SIZE);
     }
 
-    /* Assign new memory block */
+    /* search for free memory block */
     char *nd = myblock;
-    while (nd - (char *)myblock < BLOCK_SIZE) { 
+    while (nd < myblock + BLOCK_SIZE) { 
         int nd_used = get_used(nd);
         int nd_size = get_size(nd);
+
         /* Free block and enough space to accomodate request */
         if (nd_used == 0 && nd_size >= size) {
-            /* Enough space to make a new block */
-            if (nd_size > size + META_SIZE) {
+            /* Resize blocks */
+            if (nd_size - size > META_SIZE) {  /* Enough space to make a new block */
                 char *new_nd = nd + (size + META_SIZE);
                 set_used(new_nd, 0);
                 set_size(new_nd, nd_size - (size + META_SIZE));
 
                 set_used(nd, 1);
                 set_size(nd, size);
-
-                return nd+META_SIZE;
             }
-            /* Just enough space to allocate this block */
-            else {
+            else {  /* not enough room to make new block */
+                if (nd_size > size) {  /* block may have used bytes at the end */
+                    set_freed(nd, nd_size-size);
+                }
                 set_used(nd, 1);
-                return nd + META_SIZE;
             }
+            return nd + META_SIZE;
         }
 
-        else {
-            nd += nd_size + META_SIZE;
-        }
+        nd += nd_size + META_SIZE;
     }
+
     /* Error has occured */
     printf("NO SPACE\n");
     return NULL;
@@ -108,15 +123,35 @@ void myfree(char *ptr) {
             printf("Specified pointer found...clearing\n");
             
             set_used(curr, false);
+            set_freed(curr, 0);
+
             /* Merge with previous memory block if possible */
-            if (prev != NULL && get_used(prev) == false) {
-                int new_size = get_size(prev) + get_size(curr) + META_SIZE;
-                set_size(prev, new_size);
-                curr = prev;
+            if (prev != NULL) {
+                if (get_used(prev) == false) {
+                    int new_size = get_size(prev) + get_size(curr) + META_SIZE;
+                    set_size(prev, new_size);
+                    set_freed(prev, 0);
+                    curr = prev;
+                }
+                /* try to reclaim memory from prev */
+                else if (get_freed(prev) > 0) {
+                    int freed = get_freed(prev);
+
+                    set_size(prev, get_size(prev) - freed);
+                    set_freed(prev, 0);
+
+                    set_size(curr, get_size(curr) + freed);
+
+                    *(curr-freed) = *curr;  /* move metadata back */
+                    *(curr-freed+1) = *(curr+1);
+
+                    curr = curr-freed;
+                }
             }
 
-            if (curr + get_size(curr) < (myblock + BLOCK_SIZE)) {
-                /* If possible, increment nd2 to the next memory block */
+            /* Merge with next memory block if possible */
+            if (curr + get_size(curr) + META_SIZE < (myblock + BLOCK_SIZE)) {
+                /* peek next block */
                 char *peek = curr + get_size(curr) + META_SIZE;
                 if (get_used(peek) == false) {
                     /* Merge with next memory block */
@@ -134,56 +169,63 @@ void myfree(char *ptr) {
 }
 
 void print_mem() {
-    for (int i=0; i < META_SIZE; i++) {
-        printf("%d ", myblock[i]);
+    char *curr = myblock;
+    while (curr < myblock + BLOCK_SIZE) { 
+        int used = get_used(curr);
+        int size = get_size(curr);
+        int freed = get_freed(curr);
+        printf("(%d, %d, %d) -> ", used, size, freed);
+        curr += get_size(curr) + META_SIZE;
     }
     printf("\n");
 }
 
-void print_block(char *addr) {
-    int res = get_used(addr);
+void print_block(void *addr) {
+    int used = get_used(addr);
     int size = get_size(addr);
-    printf("Block at %p:\n\tUsed: %d\n\tSize: %d\n\n", addr, res, size);
+    int freed = get_freed(addr);
+
+    printf("\nBlock at 0x%p: ", addr);
+    print_bin(addr);
+    printf("\tUsed: %d\n", used);
+    printf("\tSize: %d\n", size);
+    printf("\tFreed: %d\n", freed);
+    printf("\n");
 }
 
-/* For testing purposes, in reality our functions would be included in a library */
-// int main() {
-//     // print_block(myblock);
+void print_bin(unsigned char *addr) {
+    unsigned char val1 = *addr;
+    unsigned char val2 = *(addr+1);
 
-//     mymalloc(10);
-//     print_block(myblock);
-//     mymalloc(10);
+    for (int i=7; i >= 0; i--) {
+        printf("%d", (val1 >> i) & 1);
+    }
 
-//     myfree(myblock+2);
-//     print_block(myblock);
+    printf(" ");
 
-//     return 0;
-// }
+    for (int i=7; i >= 0; i--) {
+        printf("%d", (val2 >> i) & 1);
+    }
+
+    printf("\n");
+}
+
 int main() {
-    printf("MY BLOCK: %p\n",myblock);
+    char *a = mymalloc(10);
+    char *b = mymalloc(10);
 
-    char* c = mymalloc(10);
-    printf("%p\n",c);
+    print_block(b-2);
+    myfree(a);
+    print_block(a-2);
 
-    char* d = mymalloc(10);
-    printf("%p\n",d);
+    char *c = mymalloc(8);
+    print_block(c-2);
 
-    char* e = mymalloc(10);
-    printf("%p\n",e);
+    print_mem();
 
-    char* f = mymalloc(10);
-    printf("%p\n",f);
+    myfree(b);
 
-    myfree(c);
-    myfree(e);
-    myfree(d);
-
-    print_block(myblock);
-
-    c = mymalloc(30);
-    d = mymalloc(10);
-    printf("%p\n",c);
-    printf("%p\n",d);
+    print_mem();
 
     return 0;
 }
--- mymalloc.h
+++ mymalloc.h
@@ -1,2 +1,13 @@
 //#define malloc(x) mymalloc(x,_FILE_,_LINE_)
 //#define free(x) myfree(x,_FILE_,_LINE_)
+
+bool get_used(void *addr);
+int get_size(void *addr);
+void set_used(void *addr, bool used);
+void set_size(void *addr, int size);
+
+void *mymalloc(int size);
+void myfree(char *ptr);
+
+void print_block(void *addr);
+void print_bin(unsigned char *addr);
