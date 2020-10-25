64852cd7cbfbdeead45274b823a264cce8f9ae3b
Sunny Chen <sunnychen2000@gmail.com>
2020-10-24 21:59:57 -0400

    asdfsadf

--- memgrind.c
+++ memgrind.c
@@ -54,25 +54,22 @@ void workload_c() {
 
 /* Workload D */
 void workload_d() {
-    int i;
-    char* a;
-    a = malloc(4094);
+    char *a = malloc(4094);
     free(a);
-    char* b;
-    b = malloc(4096);
+
+    char *b = malloc(4096);
     free(b);
-    int* c[682];
-    char* d; 
-    double* e;
-    for (i = 0; i < 682; i++) {  //will allocate 682*(4+2) = 4092 bytes
+
+    int *c[682];
+    for (int i = 0; i < 682; i++) {  // will allocate 682*(4+2) = 4092 bytes
         c[i] = malloc(4);
     }
-    for (i = 0; i < 682; i+=2) {  //will free every other 6 byte memory block
+    for (int i = 0; i < 682; i+=2) {  // will free every other 6 byte memory block
         free(c[i]);
     }
-    d = malloc(1); //will allocate 1+2 = 3 bytes
-    e = malloc(8); //will attempt to allocate 2+8 = 10 bytes, will fail because of memory fragmentation
-    return;
+
+    char *d = malloc(1); // will allocate 1+2 = 3 bytes
+    double *e = malloc(8); // will attempt to allocate 2+8 = 10 bytes, will fail because of memory fragmentation
 }
 
 /* Workload E */
@@ -82,18 +79,19 @@ void workload_e() {
         a[0] = malloc(1022);
         a[1] = malloc(1022);
         a[2] = malloc(1022);
-        //uses 3*1024 = 3072 bytes
+        // uses 3*1024 = 3072 bytes
         free(a[0]);
         free(a[2]);
-        free(a[1]); //freed memory blocks are now adjacent and should be merged
-        a[3] = malloc(4092); //4092 bytes allocated + 2 bytes of metadata uses 4094 bytes
-        /* 
-        last two free bytes are not sufficient to store the minimum of 1 byte of allocated memory, thus they
-        incorporated into a[3]. malloc should be unable to allocate 1 additional byte of data
-        */
+        free(a[1]); // freed memory blocks are now adjacent and should be merged
+        a[3] = malloc(4092); // 4092 bytes allocated + 2 bytes of metadata uses 4094 bytes
+
+        /* last two free bytes are not sufficient to store the minimum of 1 byte of allocated memory, thus they
+        incorporated into a[3]. malloc should be unable to allocate 1 additional byte of data */
+
         a[4] = malloc(1);
-        free(a[3]); //frees all 4096 bytes
-        a[4] = malloc(1); //allocated 1+2 = 3 bytes
+        free(a[3]); // frees all 4096 bytes
+        
+        a[4] = malloc(1); // allocated 1+2 = 3 bytes
         free(a[4]);
     }
 }
