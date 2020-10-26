4a72bd8aec99f29d8f272e5604e305b362510795
Sunny Chen <sunnychen2000@gmail.com>
2020-10-25 21:40:06 -0400

    header guard

--- mymalloc.h
+++ mymalloc.h
@@ -1,3 +1,6 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
 #include <stddef.h>
@@ -13,3 +16,5 @@ void set_hanging(const void *addr, size_t size);
 
 void *mymalloc(size_t size, const char* file, int line);
 void myfree(const void *addr, const char* file, int line);
+
+#endif
